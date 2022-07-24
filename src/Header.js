fb4e368a604143ffb45782706a6aa717b7caea0f
Ashishdev221 <84578874+Ashishdev221@users.noreply.github.com>
2022-07-24 20:27:54 +0530

    header completed

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import "./Header.css"
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 function Header() {
   return (
     <div className="header">
@@ -10,19 +12,24 @@ function Header() {
       />
       <div className="header_search">
         <input className="header_searchInput" type="text" />
+        <SearchIcon className="header_searchIcon"/>
       </div>
       <div className="header_nav">
         <div className="header_option">
-          <span className="header_optionLineOne">hello guest</span>
-          <span className="header_optionLineTwo">sign in</span>
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineTwo">Sign In</span>
         </div>
-        <div>
+        <div className="header_option">
           <span className="header_optionLineOne">Returns</span>
           <span className="header_optionLineTwo">& Orders</span>
         </div>
-        <div>
+        <div className="header_option">
           <span className="header_optionLineOne">Your</span>
           <span className="header_optionLineTwo">Prime</span>
+        </div>
+        <div className="header_optionBasket">
+          <ShoppingBasketIcon/>
+          <span className="header_optionLineTwo header_basketCount">0</span>
         </div>
       </div>
     </div>
