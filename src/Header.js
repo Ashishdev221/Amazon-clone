583155a7bb49648968fed493be721bfbda5a69f1
Ashishdev221 <84578874+Ashishdev221@users.noreply.github.com>
2022-07-28 00:58:06 +0530

    subtotal start

--- src/Header.js
+++ src/Header.js
@@ -2,17 +2,20 @@ import React from "react";
 import "./Header.css"
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header_logo"
-        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt="amazon"
-      />
+      <Link to="/">
+        <img
+          className="header_logo"
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="amazon"
+        />
+      </Link>
       <div className="header_search">
         <input className="header_searchInput" type="text" />
-        <SearchIcon className="header_searchIcon"/>
+        <SearchIcon className="header_searchIcon" />
       </div>
       <div className="header_nav">
         <div className="header_option">
@@ -27,10 +30,13 @@ function Header() {
           <span className="header_optionLineOne">Your</span>
           <span className="header_optionLineTwo">Prime</span>
         </div>
-        <div className="header_optionBasket">
-          <ShoppingBasketIcon/>
-          <span className="header_optionLineTwo header_basketCount">0</span>
-        </div>
+        <Link to="/checkout">
+          <div className="header_optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header_optionLineTwo header_basketCount">0</span>
+          </div>
+        </Link>
+
       </div>
     </div>
   );
